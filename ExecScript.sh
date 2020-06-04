dd48d3fa1e416be8eeb80ab2c2d7e7f641d7e63f
savioalexin1 <alex.william@hpe.com>
2020-06-04 19:01:13 +0530

    Update ExecScript.sh
    Checking

--- ExecScript.sh
+++ ExecScript.sh
@@ -2,4 +2,4 @@
 
 echo "[252469C9712C5E14323A052A10E0125F : low : Password Management : Password in Comment : structural ] services/PaymentPreferencesService.java(127)"
 sleep 30
-output=`wget --no-proxy http://$1:8080/`
+exit 1
