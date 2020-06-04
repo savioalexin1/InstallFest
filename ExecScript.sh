00784617ce8b1a58acd3226ce301a7d93bc71ce2
savioalexin1 <alex.william@hpe.com>
2020-06-04 18:57:31 +0530

    Update ExecScript.sh
    Changed

--- ExecScript.sh
+++ ExecScript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Install Fest Test Script : Arg1 : $1 Arg: $2"
+echo "[252469C9712C5E14323A052A10E0125F : low : Password Management : Password in Comment : structural ] services/PaymentPreferencesService.java(127)"
 sleep 30
 output=`wget --no-proxy http://$1:8080/`
